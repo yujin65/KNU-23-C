8e93d63b7c854ab3607de859432e9639ed26e4fc
yujin65 <hyj23510@naver.com>
2023-09-14 11:27:57 +0900

    grade

---
+++ 202302123_1/lecture1/lecture7.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int jumsu;
+	char grade;
+
+	scanf_s("%d", &jumsu);
+
+	if (jumsu > 100 || jumsu < 0)
+		printf("잘못 입력했습니다.\n");
+	else if (jumsu >= 90)
+		grade = 'A';
+	else if (jumsu >= 80)
+		grade = 'B';
+	else if (jumsu >= 70)
+		grade = 'C';
+	else if (jumsu >= 60)
+		grade = 'D';
+	else
+		grade = 'F';
+
+	printf("점수 등급 : %c학점\n", grade);
+	return 0;
+}
